2ef900955fc570906194496939998ea3d4a86ab4
moulikulanthaivel <moulikulanthaivel@gmail.com>
2024-01-31 05:31:22 -0800

    array learn

---
+++ array.js
@@ -0,0 +1,36 @@
+let ename=["rahul" , "rob" , "yash" , "candy"]
+console.log(ename)
+
+console.log(ename[0]);
+console.log(ename[1]);
+console.log(ename[2]);
+console.log(ename[3]);
+
+// update array
+
+ename=["fayaz"]
+console.log(ename)
+
+//for
+
+let eid=[12,13,14,15,16,17,18]
+// console.log(eid.length)
+
+for(i=0 ; i<=3; i= i+1){
+console.log(eid[i])
+}
+
+//for of
+
+let ewashs=['rahul' , 32 , true , false , {}]
+console.log(ewashs.length)
+console.log(ewashs[2])
+
+let emark=[101,102,108,-11]
+if(emark.length>0[4]){
+console.log("yes correct")
+
+}
+else{
+console.log("no its wrong")
+}
